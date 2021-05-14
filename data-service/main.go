d6a0dbbaae36ddf16d98a1ee126f548743e1dfe2
Kratos40-sba <a.khebchi@esi-sba.dz>
2021-05-14 07:14:57 +0100

    initial commit

--- data-service/main.go
+++ data-service/main.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Kratos40-sba/data-service/api"
+	"github.com/Kratos40-sba/data-service/broker"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+const (
+	ServerPort = ":8080"
+)
 
 func main() {
-	fmt.Println("Hello from data service !")
 	/*
 	 Subscribe at multiple topics
 	 Insert into influxDB
 	*/
+	router := gin.Default()
+	mqttConnection := broker.NewMqttConnection("Client-1")
+	mqttConnection.Subscribe("")
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/health", api.HealthStatusHandler(mqttConnection))
+	}
+	router.NoRoute(func(context *gin.Context) {
+		context.JSON(http.StatusNotFound, gin.H{"msg": "Route Not Defined"})
+	})
+	log.Fatalln(router.Run(ServerPort))
 }
